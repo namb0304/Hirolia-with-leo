d4c238f0c518835fcd55e961f97bed508ca91247
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-10-22 21:54:57 +0900

    ログインファイルの作成

--- app/routes/store.py
+++ app/routes/store.py
@@ -1,5 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 store_bp = Blueprint(
     "store", __name__, url_prefix="/store", template_folder="../../templates"
 )
+
+
+store_bp.route("/store/login", methods=["GET", "POST"])
+def store_login():
+    return render_template("store/login.html")
