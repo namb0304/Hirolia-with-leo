bf5e3732bc7176a0c2694b5dea05b1286ad9d137
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-10-23 15:10:00 +0900

    一旦おけ

--- app/routes/store.py
+++ app/routes/store.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, render_template
 
-store_bp = Blueprint(
-    "store", __name__, url_prefix="/store", template_folder="../../templates"
-)
+store_bp = Blueprint("store_bp", __name__, url_prefix="/store", template_folder="../../templates")
 
 
-store_bp.route("/store/login", methods=["GET", "POST"])
+@store_bp.route("/login", methods=["GET", "POST"])
 def store_login():
     return render_template("store/login.html")
+
+
+@store_bp.route("/seat_status", methods=["GET", "POST"])
+def store_seat_status():
+    return render_template("store/seat_status.html")
+
+@store_bp.route("/order_status", methods=["GET", "POST"])
+def store_order_status():
+    return render_template("store/order_status.html")
